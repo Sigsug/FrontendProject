91f4f95793496f362a141c89fc07d6e4a75a68e7
Sagi <sagi813@gmail.com>
2024-12-07 17:21:56 +0200

    Split to projects

--- entities/destination.js
+++
@@ -1,9 +0,0 @@
-class Destination {
-    constructor(destCode, destName, airportName, airportUrl, imageUrl) {
-        this.destCode = destCode;
-        this.destName = destName;
-        this.airportName = airportName;
-        this.airportUrl = airportUrl;
-        this.imageUrl = imageUrl;
-    }
-}
