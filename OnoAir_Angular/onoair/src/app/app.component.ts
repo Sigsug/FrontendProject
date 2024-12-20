585dfa6c03551ecfdc862d453ab4e7a19bb90947
Sagi <sagi813@gmail.com>
2024-12-20 21:05:59 +0200

    organized the project folders

--- OnoAir_Angular/onoair/src/app/app.component.ts
+++ OnoAir_Angular/onoair/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './Shared/header/header.component';
+import { FooterComponent } from './Shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
