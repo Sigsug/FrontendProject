15dccffc715d32ca4609c9c07f6e76d8a42c8b4c
Sagi <sagi813@gmail.com>
2024-12-20 17:47:38 +0200

    start build components

--- OnoAir_Angular/onoair/src/app/app.config.ts
+++ OnoAir_Angular/onoair/src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    MatToolbarModule,
+    MatIconModule, provideAnimationsAsync()
+  ]
 };
