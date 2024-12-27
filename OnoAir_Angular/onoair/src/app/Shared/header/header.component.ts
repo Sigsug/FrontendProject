fa66eca002693f8a7f78d9b7df4350c56de61c1a
Sigsug <sagi813@gmail.com>
2024-12-27 10:01:30 +0200

    update the header burger

--- OnoAir_Angular/onoair/src/app/Shared/header/header.component.ts
+++ OnoAir_Angular/onoair/src/app/Shared/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule, MatMenuPanel } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -18,6 +18,7 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+[x: string]: MatMenuPanel<any>|null;
 
   
 
